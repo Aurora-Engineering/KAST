4e73304d1c0082981859de5c3227aa48b2ef491e
cfirth-nasa <connor.c.firth@nasa.gov>
2023-11-01 11:49:52 -0400

    Add base telemetry class

--- driver.py
+++ driver.py
@@ -1,10 +1,15 @@
 # Knowledge Aquisition and Synthesis Tool
 
 from src.predicate import Predicate
+from src.telem.telemetry import Telemetry
+import numpy as np
 
 def main():
 	temp_pred = Predicate('at', [1,2,3])
+	temp_arr = np.array([(1,2),(3,4)])
+	temp_telem = Telemetry('Numpy Array',type(temp_arr),temp_arr)
 	print(temp_pred)
+	print(temp_telem)
 	return 
 
 if __name__ == '__main__':
